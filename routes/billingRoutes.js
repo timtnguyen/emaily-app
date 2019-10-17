7157b68aba83d659594fc0c58142b6f29f8b9761
Thinh Nguyen <nguyen710150@yahoo.com>
2019-10-16 22:52:43 -0700

    another commit

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -3,8 +3,8 @@ const stripe = require("stripe")(keys.stripeSecretKey);
 const requireLogin = require("../middlewares/requireLogin");
 
 module.exports = app => {
-  app.post("/api/stripe", requireLogin, async (req, res) => {
-    const charge = await stripe.charges.create({
+  app.post("/api/stripe", requireLogin, (req, res) => {
+    const charge = stripe.charges.create({
       amount: 500,
       currency: "usd",
       description: "$5 for 5 credits",
@@ -12,7 +12,7 @@ module.exports = app => {
     });
 
     req.user.credits += 5;
-    const user = await req.user.save();
+    const user = req.user.save();
 
     res.send(user);
   });
